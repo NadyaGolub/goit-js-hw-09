e80d8d9644465da73bc9d1029218e2b5f418c7d7
NadyaGolub <nadyagolub@ukr.net>
2023-01-07 12:48:07 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  }, 1000);
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  
+});
